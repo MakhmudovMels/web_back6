0f14fe7015e9fa7b96c5e1e2fca5a957e25e15a5
Mels <mels.mahmudov@gmail.com>
2022-04-24 14:33:03 +0300

    list of users

--- adminka/admin.php
+++ adminka/admin.php
@@ -33,6 +33,72 @@ $count2 = $stmt->rowCount();
 $stmt = $db->prepare("SELECT * FROM superability WHERE name_of_superability = ?");
 $stmt -> execute(["Левитация"]);
 $count3 = $stmt->rowCount();
+
+$stmt = $db->query("SELECT max(id) FROM human");
+$row = $stmt->fetch();
+$count = (int) $row[0];//Берем максимальный айди среди пользователей для заполнения списка пользователей
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_user'])){//Если была нажата кнопка удалить пользователя
+
+  if($_POST['select_user'] == 0){//Обработчик того был ли выбран пользователь
+      header('Location: admin.php');
+  }
+
+  $user_id =  mysqli_real_escape_string($connect ,$_POST['select_user']);//Получение айди выбраного польвователя
+
+  //Удаление выбранного пользователя
+  $sql = "DELETE FROM users WHERE id = '$user_id'";
+  mysqli_query($connect, $sql);
+  //Удаление всех выбраных им суперспособностей
+  $sql = "DELETE FROM super_power WHERE id = '$user_id'";
+  mysqli_query($connect, $sql);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['edit'])){//Если была нажата кнопка изменить данные пользователя
+
+  if($_POST['select_user'] == 0){//Обработчик того был ли выбран пользователь
+     header('Location: adminroom.php');
+  }
+  //Берем данные об измененных способностях
+  $power1=in_array('s1',$_POST['capabilities']) ? '1' : '0';
+  $power2=in_array('s2',$_POST['capabilities']) ? '1' : '0';
+  $power3=in_array('s3',$_POST['capabilities']) ? '1' : '0';
+  $power4=in_array('s4',$_POST['capabilities']) ? '1' : '0';
+
+  //Способности сохраняются в единную строку которая позже будет сохранена в бд
+  if($power1 == 1){
+      $ability = 'immortal' . ',';
+  }
+
+  if($power2 == 1 && !empty($ability)){
+      $ability .= 'noclip' . ',';
+  }else if($power2 == 1 && empty($ability)){
+      $ability = 'noclip' . ',';
+  }
+
+  if($power3 == 1 && !empty($ability)){
+      $ability .= 'flying' . ',';
+  }else if($power3 == 1 && empty($ability)){
+      $ability = 'flying' . ',';
+  }
+
+  if($power4 == 1 && !empty($ability)){
+      $ability .= 'lazer' . ',';
+  }else if($power4 == 1 && empty($ability)){
+      $ability = 'lazer' . ',';
+  }
+
+  //Блок изменения данных пользователя введеных админом
+  $id = $_COOKIE['id'];
+
+  $stmt = $db->prepare("UPDATE users SET name = ?, mail = ?, bio = ?, date = ?, gender = ?, limbs = ? WHERE id = ?");
+  $stmt -> execute(array($_POST['name'],$_POST['email'],$_POST['bio'],$_POST['year'],$_POST['gender'],$_POST['limbs'], $id));
+
+  $stmt = $db->prepare("UPDATE  super_power SET superabilities = ? WHERE human_id = ?");
+  $stmt -> execute([$ability,$id]);
+
+  setcookie('id','',1);
+}
 ?>
 
 <!DOCTYPE html>
@@ -41,12 +107,27 @@ $count3 = $stmt->rowCount();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="admin.css">
+  <title>Админка</title>
 </head>
 <body>
   <h2>Статистика по суперспособностям</h2>
   <section>Бессмертие: <?php print $count1 ?></section> <br>
   <section>Прохождение сквозь стены: <?php print $count2 ?></section> <br>
   <section>Левитация: <?php print $count3 ?></section> <br>
+
+  <select name="select_user" class ="slc_user" id="selector_user">
+    <option selected disabled value ="0">Выбрать пользователя</option>
+    <?php
+    for($index =1 ;$index <= $count;$index++){//Заполнение списка пользователями
+      $stmt = $db->prepare("SELECT * FROM human WHERE id = ?");
+      $stmt -> execute([$index]);
+      $user = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($user['id'] == $index){//Проверка на существование пользователя с айди index
+          print("<option value =" . $index . ">" . "id : ". $user['id'] . " Имя : " . $user['name'] . "</option>");//Добавление в список пользователя с существующим айди
+      }
+    }
+    ?>
+  </select>
 </body>
 </html>
