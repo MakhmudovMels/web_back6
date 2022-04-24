0910dfa7b2c4288130357ad5f8180ab5bb3e416d
Mels <mels.mahmudov@gmail.com>
2022-04-24 12:01:04 +0300

    first commit

---
+++ adminka/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Задача 6. Реализовать вход администратора с использованием
+ * HTTP-авторизации для просмотра и удаления результатов.
+ **/
+
+// PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
+// Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
+
+$db = new PDO('mysql:host=localhost;dbname=u46502', 'u46502', '3119750', array(PDO::ATTR_PERSISTENT => true));
+$stmt = $db->prepare("SELECT * FROM admin WHERE id = 1");
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != $row['login'] ||
+    md5($_SERVER['PHP_AUTH_PW']) != $row['pass']) {
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
+
+print('Вы успешно авторизовались и видите защищенные паролем данные.');
+
+// *********
+// Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
+// Реализовать просмотр и удаление всех данных.
+// *********
