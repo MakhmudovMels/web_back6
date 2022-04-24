47a7ed03ec917eddb7d9ea7404688fcb768f7087
Mels <mels.mahmudov@gmail.com>
2022-04-24 13:02:44 +0300

    superability

--- adminka/admin.php
+++ adminka/admin.php
@@ -22,9 +22,31 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
   exit();
 }
 
-print('Вы успешно авторизовались и видите защищенные паролем данные.');
+// успешно авторизовались и видим защищенные паролем данные
+// собирамем статистику по суперспособностям
+$stmt = $db->prepare("SELECT * FROM superability WHERE name_of_superability = ?");
+$stmt -> execute(["Бессмертие"]);
+$count1 = $stmt->rowCount();
+$stmt = $db->prepare("SELECT * FROM superability WHERE name_of_superability = ?");
+$stmt -> execute(["Прохождение сквозь стены"]);
+$count2 = $stmt->rowCount();
+$stmt = $db->prepare("SELECT * FROM superability WHERE name_of_superability = ?");
+$stmt -> execute(["Левитация"]);
+$count3 = $stmt->rowCount();
+?>
 
-// *********
-// Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
-// Реализовать просмотр и удаление всех данных.
-// *********
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>Статистика по суперспособностям</h2>
+  <section>Бессмертие: <?php print $count1 ?></section> <br>
+  <section>Прохождение сквозь стены: <?php print $count2 ?></section> <br>
+  <section>Левитация: <?php print $count3 ?></section> <br>
+</body>
+</html>
